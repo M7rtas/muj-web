d98871ed41213cdb11b33981e8a5ae03ddc6dad4
Martin <martin.svacina13@gmail.com>
2026-07-18 21:29:56 +0200

    aktualizace

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-<p style= "text-align: center;"> ahoj </p>
+<!DOCTYPE html>
+<html>
+<body>
+
+<input type="number" id="textik" oninput="reaguj()" placeholder="Napiš číslo">
+
+<p id="vystup"></p>
+
+<script>
+  function reaguj() {
+    let text = document.getElementById("textik").value;
+
+    if (text < 5) {
+      document.getElementById("vystup").innerText = "míň ⬇️";
+    } else if (text == 5) {
+      document.getElementById("vystup").innerText = "rovno 5 🎯";
+    } else {
+      document.getElementById("vystup").innerText = "víc ⬆️";
+    }
+  }
+</script>
