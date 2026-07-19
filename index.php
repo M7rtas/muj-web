4367887263221ad2de945dfb4001849ac8b1137a
Martin <martin.svacina13@gmail.com>
2026-07-19 11:38:43 +0200

    první verze webu

--- index.php
+++ index.php
@@ -1,21 +1,8 @@
 <!DOCTYPE html>
 <html>
 <body>
-
-<input type="number" id="textik" oninput="reaguj()" placeholder="Napiš číslo">
-
-<p id="vystup"></p>
-
+<p>ahoj</p>
+</body>
 <script>
-  function reaguj() {
-    let text = document.getElementById("textik").value;
 
-    if (text < 5) {
-      document.getElementById("vystup").innerText = "míň ⬇️";
-    } else if (text == 5) {
-      document.getElementById("vystup").innerText = "rovno 5 🎯";
-    } else {
-      document.getElementById("vystup").innerText = "víc ⬆️";
-    }
-  }
 </script>
