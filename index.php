1df44f1ea4926936fd3124502f01b6bdb6026d35
Martin <martin.svacina13@gmail.com>
2026-07-19 11:56:53 +0200

    aktualizace

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html>
 <body>
-<p>ačus</p>
+<button id="myButton" onclick="prepniObrazek()">Click me</button>
 </body>
+<img src="emoji.png" alt="Emoji" id="myImage">
 <script>
-
+function prepniObrazek() {
+    var image = document.getElementById("myImage");
+    if (image.src.includes("emoji.png")) {
+        image.src = "new_image.png";
+    } else {
+        image.src = "emoji.png";
+    }
+}
 </script>
