015af904f786fadb6f4699191a1f5565134da8f9
Martin <martin.svacina13@gmail.com>
2026-07-19 13:01:18 +0200

    aktualizace

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="style.css">
 <html>
 <body>
-<input type="number" placeholder="cislo" id="cislo" oninput="porovnej()">
+<input class="nahoreuprostred" type="number" placeholder="cislo" id="cislo" oninput="porovnej()">
+<div id="vysledek"></div>
 </body>
 
 <script>
 function porovnej() {
   var cislo = document.getElementById("cislo").value;
   if (cislo > 10) {
-    alert("Číslo je větší");
-  } else if (cislo < 10) {
-    alert("Číslo je menší");
-  } else {
-    alert("Číslo je rovno 10");
+    document.getElementById("vysledek").innerHTML = '<h1 class="uprostred">Číslo je menší</h1>';
+} else if (cislo < 10) {
+    document.getElementById("vysledek").innerHTML = '<h1 class="uprostred">Číslo je větší</h1>';
+  } else if (cislo === "") {
+    document.getElementById("vysledek").innerHTML = '<h1 class="uprostred">Zadejte číslo</h1>';
+  }
+  
+  else {
+    document.getElementById("vysledek").innerHTML = '<h1 class="uprostred">Číslo je 10</h1>';
   }
 }
 </script>
