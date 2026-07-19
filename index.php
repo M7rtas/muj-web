fc7a06944eac0b83733c1da3268d4303d8699d1a
Martin <martin.svacina13@gmail.com>
2026-07-19 13:06:32 +0200

    aktualizace

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
 function porovnej() {
   var cislo = document.getElementById("cislo").value;
   if (cislo > 10) {
-    document.getElementById("vysledek").innerHTML = '<h1 class="uprostred">Číslo je menší</h1>';
+    document.getElementById("vysledek").innerHTML = '<h1 class="stred">Číslo je menší</h1>';
 } else if (cislo < 10) {
-    document.getElementById("vysledek").innerHTML = '<h1 class="uprostred">Číslo je větší</h1>';
-  } else if (cislo === "") {
-    document.getElementById("vysledek").innerHTML = '<h1 class="uprostred">Zadejte číslo</h1>';
+    document.getElementById("vysledek").innerHTML = '<h1 class="stred">Číslo je větší</h1>';
+  } else if (cislo = "") {
+    document.getElementById("vysledek").innerHTML = '<h1 class="stred">Zadejte číslo</h1>';
   }
   
   else {
-    document.getElementById("vysledek").innerHTML = '<h1 class="uprostred">Číslo je 10</h1>';
+    document.getElementById("vysledek").innerHTML = '<h1 class="stred">Číslo je 10</h1>';
   }
 }
 </script>
