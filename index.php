3f0aa82c67fab932543b0375d5a1119896555901
Martin <martin.svacina13@gmail.com>
2026-07-19 12:03:12 +0200

    aktualizace

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <body>
 <button id="myButton" onclick="prepniObrazek()">Click me</button>
 </body>
-<img src="emoji.png" alt="Emoji" id="myImage">
+<img src="emoji.png" alt="Emoji" id="myImage" scale="0.5" width="100" height="100" img style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
 <script>
 function prepniObrazek() {
     var image = document.getElementById("myImage");
